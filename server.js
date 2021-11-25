1ec9c64dea0c19bcffba93304d49bc6237e1bfdf
olive <emailoxc@gmail.com>
2021-11-25 01:10:29 -0500

    Added status to response

--- server.js
+++ server.js
@@ -42,13 +42,15 @@ app.get("/app/user/:id", (req, res) => {
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id = ?").run(req.params.user, req.params.pass, req.params.id);
-	res.status(200).json(stmt);
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id = ?")
+	const run = stmt.run(...req.params);
+	res.status(200).json(run);
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run(req.params.id);
+	res.status(200).json(stmt);
 });
 
 // Default response for any other request
